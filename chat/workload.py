6185debbb33c81704fefcab9edde44fdc8d09096
root <root@localhost>
2024-12-10 13:31:36 -0700

    async client workload initial version of timing

--- chat/workload.py
+++ chat/workload.py
@@ -30,23 +30,38 @@ def add_message(room_id, from_id, content, timestamp):
 
 def create():
     num_minutes = 1
+    api = ['create_user', 'create_private_room', 'add_message', 'get_messages']
     t_end = time.time() + 60 * num_minutes
+    file1 = open("/users/akalaba/asynch_latencies.txt", "w+")
+    selector = 0
     while time.time() < t_end:
         app_request_type = random.randint(1, 100) 
+        before = time.time_ns()
         if app_request_type <= 2:
+            selector = 0
             user = np.random.zipf(2, 1)
             password = np.random.zipf(2, 1)
             utils.create_user(str(user), str(password))
-        elif app_request_type <= 8:
+        elif app_request_type <= 10:
+            selector = 1
             user1 = np.random.zipf(2, 1)
             user2 = np.random.zipf(2, 1)
             utils.create_private_room(user1, user2)
-        elif app_request_type <= 40:
+        elif app_request_type <= 50:
+            selector = 2
             room_id = int(np.random.zipf(2, 1))
             from_id = 44
             content = 'heyyy'
             timestamp = time.time()
             add_message(room_id, from_id, content, timestamp)
         else:
+            selector = 3
             room_id = np.random.zipf(2, 1)
             utils.get_messages(room_id)
+        
+        after = time.time_ns()
+        lat = after - before
+        optype = api[selector]
+        file1.write(f'app,{lat}')
+        file1.write(f'{optype},{lat}')
+
