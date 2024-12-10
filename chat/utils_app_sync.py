1d13c67cd8513edc179c5a6d664e94ce30c46cdd
Anja Kalaba <akalaba@localhost>
2024-12-10 11:56:11 -0700

    nontransforemd version

---
+++ chat/utils_app_sync.py
@@ -0,0 +1,72 @@
+import asyncio
+import json
+import math
+import random
+import bcrypt
+from chat import demo_app_sync as demo_data
+from chat.config import get_config
+from mdlin import SyncAppRequest
+
+SERVER_ID = random.uniform(0, 322321)
+redis_client = get_config().redis_client
+
+def make_username_key(username):
+    return f'username:{username}'
+
+def create_user(username, password):
+    username_key = make_username_key(username)
+    hashed_password = bcrypt.hashpw(str(password).encode('utf-8'), bcrypt.gensalt(10))
+    next_id = SyncAppRequest('INCR', 'total_users')
+    user_key = f'user:{next_id}'
+    SyncAppRequest('SET', username_key, user_key)
+    SyncAppRequest('HMSET', user_key, {'username': username, 'password': hashed_password})
+    SyncAppRequest('SADD', f'user:{next_id}:rooms', '0')
+    return {'id': next_id, 'username': username}
+
+def get_messages(room_id=0, offset=0, size=50):
+    """Check if room with id exists; fetch messages limited by size"""
+    room_key = f'room:{room_id}'
+    room_exists = SyncAppRequest('EXISTS', room_key)
+    if not room_exists:
+        return []
+    else:
+        values = SyncAppRequest('ZREVRANGE', room_key, offset, offset + size)
+        return list(map(lambda x: json.loads(x.decode('utf-8')), values))
+
+def hmget(key, key2):
+    """Wrapper around hmget to unpack bytes from hmget"""
+    result = SyncAppRequest('HMGET', key, key2)
+    return list(map(lambda x: x.decode('utf-8'), result))
+
+def get_private_room_id(user1, user2):
+    if math.isnan(user1) or math.isnan(user2) or user1 == user2:
+        return None
+    min_user_id = user2 if user1 > user2 else user1
+    max_user_id = user1 if user1 > user2 else user2
+    return f'{min_user_id}:{max_user_id}'
+
+def create_private_room(user1, user2):
+    """Create a private room and add users to it"""
+    room_id = get_private_room_id(user1, user2)
+    if not room_id:
+        return (None, True)
+    SyncAppRequest('SADD', f'user:{user1}:rooms', room_id)
+    SyncAppRequest('SADD', f'user:{user2}:rooms', room_id)
+    user1 = hmget(f'user:{user1}', 'username')
+    user2 = hmget(f'user:{user2}', 'username')
+    return ({'id': room_id, 'names': [user1, user2]}, False)
+
+def init_redis():
+    total_users_exist = SyncAppRequest('EXISTS', 'total_users')
+    if not total_users_exist:
+        SyncAppRequest('SET', 'total_users', 0)
+        SyncAppRequest('SET', f'room:0:name', 'General')
+        demo_data.create()
+
+def event_stream():
+    """Handle message formatting, etc."""
+    SyncAppRequest('SUBSCRIBE', 'MESSAGES')
+    messages = SyncAppRequest('LISTEN')
+    for message in messages:
+        data = f'data: {str(message)}\n\n'
+        yield data
