a2d218dd6937c38667c64129caac4f65632246bb
Austin <0austinli4@gmail.com>
2025-02-09 02:21:01 +0800

    update IOC transform

--- chat/utils.py
+++ chat/utils.py
@@ -5,50 +5,55 @@ import random
 import bcrypt
 from chat import demo_data, workload
 from chat.config import get_config
+
 SERVER_ID = random.uniform(0, 322321)
 redis_client = get_config().redis_client
 from mdlin import AppRequest, AppResponse
 
 
 def make_username_key(username):
-    return f'username:{username}'
+    return f"username:{username}"
+
 
 def create_user(username, password):
     pending_awaits = {*()}
     username_key = make_username_key(username)
-    hashed_password = bcrypt.hashpw(str(password).encode('utf-8'), bcrypt.gensalt(10))
-    
+    hashed_password = bcrypt.hashpw(str(password).encode("utf-8"), bcrypt.gensalt(10))
+
     # Convert bytes to string for storage
-    hashed_password_str = hashed_password.decode('utf-8')
-    
-    future_0 = AppRequest('INCR', 'total_users')
-    
+    hashed_password_str = hashed_password.decode("utf-8")
+
+    future_0 = AppRequest("INCR", "total_users")
+
     pending_awaits.add(future_0)
-    
+
     next_id = AppResponse(future_0)
-    
+
     pending_awaits.remove(future_0)
-    
-    user_key = f'user:{next_id}'
-    
-    future_1 = AppRequest('SET', username_key, user_key)
+
+    user_key = f"user:{next_id}"
+
+    future_1 = AppRequest("SET", username_key, user_key)
     pending_awaits.add(future_1)
-    
-    future_2 = AppRequest('HMSET', user_key, {'username': username, 'password': hashed_password_str})
+
+    future_2 = AppRequest(
+        "HMSET", user_key, {"username": username, "password": hashed_password_str}
+    )
     pending_awaits.add(future_2)
-    
-    future_3 = AppRequest('SADD', f'user:{next_id}:rooms', '0')
+
+    future_3 = AppRequest("SADD", f"user:{next_id}:rooms", "0")
     pending_awaits.add(future_3)
 
     for future in pending_awaits:
         AppResponse(future)
 
-    return (pending_awaits, {'id': next_id, 'username': username})
+    return (pending_awaits, {"id": next_id, "username": username})
+
 
 def get_messages(room_id=0, offset=0, size=50):
     pending_awaits = {*()}
-    room_key = f'room:{room_id}'
-    future_0 = AppRequest('EXISTS', room_key)
+    room_key = f"room:{room_id}"
+    future_0 = AppRequest("EXISTS", room_key)
     pending_awaits.add(future_0)
     room_exists = AppResponse(future_0)
     pending_awaits.remove(future_0)
@@ -57,81 +62,88 @@ def get_messages(room_id=0, offset=0, size=50):
             AppResponse(future)
         return (pending_awaits, [])
     else:
-        future_1 = AppRequest('ZREVRANGE', room_key, offset, offset + size)
+        future_1 = AppRequest("ZREVRANGE", room_key, offset, offset + size)
         pending_awaits.add(future_1)
         values = AppResponse(future_1)
         pending_awaits.remove(future_1)
 
         for future in pending_awaits:
             AppResponse(future)
-        return (pending_awaits, list(map(lambda x: json.loads(x.decode('utf-8')), values)))
-    
+        return (
+            pending_awaits,
+            list(map(lambda x: json.loads(x.decode("utf-8")), values)),
+        )
+
     for future in pending_awaits:
         AppResponse(future)
     return (pending_awaits, None)
 
+
 def hmget(key, key2):
     pending_awaits = {*()}
-    'Wrapper around hmget to unpack bytes from hmget'
-    future_0 = AppRequest('HMGET', key, key2)
+    "Wrapper around hmget to unpack bytes from hmget"
+    future_0 = AppRequest("HMGET", key, key2)
     pending_awaits.add(future_0)
     result = AppResponse(future_0)
     pending_awaits.remove(future_0)
-    return (pending_awaits, list(map(lambda x: x.decode('utf-8'), result)))
+    return (pending_awaits, list(map(lambda x: x.decode("utf-8"), result)))
+
 
 def get_private_room_id(user1, user2):
     if math.isnan(user1) or math.isnan(user2) or user1 == user2:
         return None
     min_user_id = user2 if user1 > user2 else user1
     max_user_id = user1 if user1 > user2 else user2
-    return f'{min_user_id}:{max_user_id}'
+    return f"{min_user_id}:{max_user_id}"
+
 
 def create_private_room(user1, user2):
     pending_awaits = {*()}
-    'Create a private room and add users to it'
+    "Create a private room and add users to it"
     room_id = get_private_room_id(user1, user2)
     if not room_id:
         return (pending_awaits, (None, True))
-    future_0 = AppRequest('SADD', f'user:{user1}:rooms', room_id)
+    future_0 = AppRequest("SADD", f"user:{user1}:rooms", room_id)
     pending_awaits.add(future_0)
-    future_1 = AppRequest('SADD', f'user:{user2}:rooms', room_id)
+    future_1 = AppRequest("SADD", f"user:{user2}:rooms", room_id)
     pending_awaits.add(future_1)
-    pending_awaits_hmget, user1 = hmget(f'user:{user1}', 'username')
+    pending_awaits_hmget, user1 = hmget(f"user:{user1}", "username")
     pending_awaits.update(pending_awaits_hmget)
-    pending_awaits_hmget, user2 = hmget(f'user:{user2}', 'username')
+    pending_awaits_hmget, user2 = hmget(f"user:{user2}", "username")
     pending_awaits.update(pending_awaits_hmget)
     for future in pending_awaits:
         AppResponse(future)
-    return (pending_awaits, ({'id': room_id, 'names': [user1, user2]}, False))
+    return (pending_awaits, ({"id": room_id, "names": [user1, user2]}, False))
+
 
 def init_redis(clientid):
     pending_awaits = {*()}
     if int(clientid) == 0:
-        future_0 = AppRequest('EXISTS', 'total_users')
+        future_0 = AppRequest("EXISTS", "total_users")
         pending_awaits.add(future_0)
         total_users_exist = AppResponse(future_0)
         pending_awaits.remove(future_0)
-        #print("HEY")
-        if total_users_exist == '0':
-            future_1 = AppRequest('SET', 'total_users', 0)
+        # print("HEY")
+        if total_users_exist == "0":
+            future_1 = AppRequest("SET", "total_users", 0)
             pending_awaits.add(future_1)
-            future_2 = AppRequest('SET', f'room:0:name', 'General')
+            future_2 = AppRequest("SET", f"room:0:name", "General")
             pending_awaits.add(future_2)
             AppResponse(future_1)
             AppResponse(future_2)
             pending_awaits.remove(future_1)
             pending_awaits.remove(future_2)
-            #pending_awaits_create, _ = demo_data.create()
-            #pending_awaits_create, _ = workload.create()
+            # pending_awaits_create, _ = demo_data.create()
+            # pending_awaits_create, _ = workload.create()
             workload.create(clientid)
             return
     elif int(clientid) > 0:
-        future_0 = AppRequest('EXISTS', 'total_users')
+        future_0 = AppRequest("EXISTS", "total_users")
         pending_awaits.add(future_0)
-        #total_users_exist = AppResponse(future_0)
-        #pending_awaits.remove(future_0)
+        # total_users_exist = AppResponse(future_0)
+        # pending_awaits.remove(future_0)
         print("HI")
-        '''while True:
+        """while True:
             pending_awaits.add(future_0)
             total_users_exist = AppResponse(future_0)
             pending_awaits.remove(future_0)
@@ -139,19 +151,20 @@ def init_redis(clientid):
             if total_users_exist != '0':
                 break
         workload.create(clientid)
-        '''
+        """
         return
+
 
 def event_stream():
     pending_awaits = {*()}
-    'Handle message formatting, etc.'
-    future_0 = AppRequest('SUBSCRIBE', 'MESSAGES')
+    "Handle message formatting, etc."
+    future_0 = AppRequest("SUBSCRIBE", "MESSAGES")
     pending_awaits.add(future_0)
-    future_1 = AppRequest('LISTEN')
+    future_1 = AppRequest("LISTEN")
     pending_awaits.add(future_1)
     messages = AppResponse(future_1)
     pending_awaits.remove(future_1)
     for message in messages:
-        data = f'data: {str(message)}\n\n'
+        data = f"data: {str(message)}\n\n"
         yield data
     return (pending_awaits, None)
--- chat/workload.py
+++ chat/workload.py
@@ -7,37 +7,52 @@ import random
 import time
 from mdlin import AppRequest, AppResponse
 
-demo_users = ['Pablo', 'Joe', 'Mary', 'Alex']
-greetings = ['Hello', 'Hi', 'Yo', 'Hola']
-demo_password = 'password123'
-messages = ['Hello!', 'Hi, How are you? What about our next meeting?', 'Yeah everything is fine', 'Next meeting tomorrow 10.00AM', "Wow that's great"]
+demo_users = ["Pablo", "Joe", "Mary", "Alex"]
+greetings = ["Hello", "Hi", "Yo", "Hola"]
+demo_password = "password123"
+messages = [
+    "Hello!",
+    "Hi, How are you? What about our next meeting?",
+    "Yeah everything is fine",
+    "Next meeting tomorrow 10.00AM",
+    "Wow that's great",
+]
+
 
 def math_random():
     return random.uniform(0, 1)
 
+
 def get_greeting():
     return greetings[math.floor(math_random() * len(greetings))]
 
+
 def add_message(room_id, from_id, content, timestamp):
     pending_awaits = {*()}
-    room_key = f'room:{room_id}'
-    message = {'from': from_id, 'date': timestamp, 'message': content, 'roomId': room_id}
+    room_key = f"room:{room_id}"
+    message = {
+        "from": from_id,
+        "date": timestamp,
+        "message": content,
+        "roomId": room_id,
+    }
 
-    future_0 = AppRequest('ZADD', room_key, {json.dumps(message): int(message['date'])})
+    future_0 = AppRequest("ZADD", room_key, {json.dumps(message): int(message["date"])})
     pending_awaits.add(future_0)
     for future in pending_awaits:
         AppResponse(future)
     return (pending_awaits, None)
 
+
 def create(clientid):
     num_minutes = 1
-    api = ['create_user', 'create_private_room', 'add_message', 'get_messages']
+    api = ["create_user", "create_private_room", "add_message", "get_messages"]
     t_end = time.time() + 60 * num_minutes
-    #file1 = open("/users/akalaba/asynch_latencies.txt", "w+")
+    # file1 = open("/users/akalaba/asynch_latencies.txt", "w+")
     selector = 0
-    #print("client-d = %s", clientid)
+    # print("client-d = %s", clientid)
     while time.time() < t_end:
-        app_request_type = random.randint(1, 100) 
+        app_request_type = random.randint(1, 100)
         before = time.time_ns()
         if app_request_type <= 2:
             selector = 0
@@ -53,19 +68,18 @@ def create(clientid):
             selector = 2
             room_id = int(np.random.zipf(2, 1))
             from_id = 44
-            content = 'heyyy'
+            content = "heyyy"
             timestamp = time.time()
             add_message(room_id, from_id, content, timestamp)
         else:
             selector = 3
             room_id = np.random.zipf(2, 1)
             utils.get_messages(room_id)
-        
+
         after = time.time_ns()
         lat = after - before
         optype = api[selector]
-        #file1.write(f'app,{lat}')
-        #file1.write(f'{optype},{lat}')
-        print(f'app,{lat}')
-        print(f'{optype},{lat}')
-
+        # file1.write(f'app,{lat}')
+        # file1.write(f'{optype},{lat}')
+        print(f"app,{lat}")
+        print(f"{optype},{lat}")
